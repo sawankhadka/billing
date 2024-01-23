06db4557762e7510d7ac4a94fe64cc754cc1841d
Sawan <144707596+sawankhadka@users.noreply.github.com>
2024-01-23 14:29:57 +0530

    Add files via upload

---
+++ fruit billing.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If grapes.Checked = True Then
+            TextBox2.Text = Val(TextBox1.Text) * 10
+            Label3.Text = "the total price is:" + TextBox2.Text
+        ElseIf RadioButton2.Checked = True Then
+            TextBox2.Text = Val(TextBox1.Text) * 12
+            Label3.Text = "the total price is:" + TextBox2.Text
+        ElseIf RadioButton3.Checked = True Then
+            TextBox2.Text = Val(TextBox1.Text) * 16
+            Label3.Text = "the total price is:" + TextBox2.Text
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Exit()
+    End Sub
+End Class
